d1a195ef0d2bacf1e4d567dc020111001a95cb88
MGCONSTRUCOESENGENHARIA <mg@mgconstrucoesengenharia.com.br>
2026-03-31 14:21:10 -0300

    fix: desabilitar cache vercel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate' },
+      ],
+    },
+  ],
 }
 module.exports = nextConfig
